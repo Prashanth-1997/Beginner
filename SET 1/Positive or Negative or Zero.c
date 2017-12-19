d64c18c6d01c7ef6d3aef3b0cf5ac519d63ded7d
Prashanth097 <34674059+Prashanth097@users.noreply.github.com>
2017-12-19 13:14:26 +0530

    Create Positive or Negative or Zero.c

---
+++ SET 1/Positive or Negative or Zero.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int num=100;
+ if(num>0)
+  printf("The number %d is EVEN",num);
+ else if(num<0)
+  printf("The number %d is ODD",num);
+ else
+  printf("It is ZERO");
+ return 0; 
+}
