5b463cde0711d0cdf430404d0c296ee8fb6d7340
Prashanth097 <34674059+Prashanth097@users.noreply.github.com>
2017-12-20 22:14:34 +0530

    Create Odd numbers between intervals.c

---
+++ SET 2/Odd numbers between intervals.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int start_num=10;
+int end_num=20;
+
+ for(start_num=10; start_num<=end_num; start_num++)
+   {
+    if(start_num%2!=0)
+     printf("%d\n",start_num);
+   }
+return 0;
+}
