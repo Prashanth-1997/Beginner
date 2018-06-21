f7a8ae8c0bee3ed6d8759a0ce5ffc13ebeae3b94
Prashanth Prakash <34674059+Prashanth-1997@users.noreply.github.com>
2018-06-21 17:29:52 +0530

    Update Program to print HELLO 5 times.c

--- SET 1/Program to print HELLO 5 times.c
+++ SET 1/Program to print HELLO 5 times.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 int main(void)
 {
-  int i,num;
-  scanf("%d",&num);
-  for(i=1;i<=num;i++,printf("\n"))
+  int j,number;
+  scanf("%d",&number);
+  for(j=1;j<=number;j++,printf("\n"))
   {
   	printf("HELLO");
   }
